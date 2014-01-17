75bbc18d4da1b60dff2e4ca7a8d313d97009adb3
avantez777 <walison_civil@yahoo.com.br>
2014-01-17 12:20:30 -0200

    line changed

---
+++ scoreCounter.lua
@@ -0,0 +1,29 @@
+-- ScoreCounter
+-- Example of score rolling counter
+
+scoreCounter = Core.class(Sprite)
+
+function scoreCounter:init(cw,ch,score)
+
+
+
+self:addEventListener(Event.ADDED_TO_STAGE, self.onAddedToStage, self)
+self:addEventListener(Event.REMOVED_FROM_STAGE, self.onRemovedFromStage, self)
+
+
+end
+
+function scoreCounter:onEnterFrame(event)
+
+
+
+end
+
+function scoreCounter:onAddedToStage(event)
+
+end
+
+function scoreCounter:onRemovedFromStage(event)
+
+
+end
